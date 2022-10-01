dac81ce38cf9ef4a57edf399a955ed06e8dc4c95
razeta <sergraz@uabc.edu.mx>
2022-09-30 19:57:22 -0700

    Update DataLoadJob.php
    Added Log register funciont.

--- DataLoadJob.php
+++ DataLoadJob.php
@@ -1,7 +1,13 @@
 <?php
-include "FileLoader.php";
+
 
 FileLoader::dirloader();
+
+//Something to write to txt log
+$log  = "Load File".' - '.date("F j, Y, g:i a").PHP_EOL.        
+        "-------------------------".PHP_EOL;
+//Save string to log, use FILE_APPEND to append.
+file_put_contents('./log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
 
 
 ?>
